c5b11a719f2afa6f3685181c355c11b1bdf48c1e
lUCI <lusindisontanjana@gmail.com>
2022-11-17 11:35:43 +0200

    Add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import store from './redux/configureStore';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  const linkElement = screen.getByText(/Space Traveler/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
 import Header from '../components/Header';
 
 describe('Test for Header components', () => {
   it('Check if the Header section are available', () => {
-    const Header = renderer.create(
+    const header = renderer.create(
       <Provider store={store}>
-        <Header />
+        <Router>
+          <Header />
+        </Router>
       </Provider>,
     );
-    expect(Header).toMatchSnapshot();
+    expect(header).toMatchSnapshot();
   });
 });
